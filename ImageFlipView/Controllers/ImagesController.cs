33678358cd256ef31b00bfb1e7a4d761c4238031
dotnetcurry <dotnetcurry@a2zknowledgevisuals.com>
2013-05-24 07:58:01 +0530

    Code Commit

---
+++ ImageFlipView/Controllers/ImagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ImageFlipView.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        public IList<string> Get()
+        {
+            return new List<string>{
+            "/Images/1.jpg",
+            "/Images/2.jpg",
+            "/Images/3.jpg",
+            "/Images/4.jpg",
+            "/Images/5.jpg",
+            "/Images/7.jpg",
+            "/Images/8.jpg",
+            "/Images/9.jpg",
+            "/Images/10.jpg",
+            "/Images/11.jpg"};
+        }
+    }
+}
---
+++ ImageFlipView/Scripts/flip-viewer.js
@@ -0,0 +1,81 @@
+﻿/// <reference path="_references.js" />
+
+function viewModel()
+{
+    var _this = this;
+    this.currentImageIndex = ko.observable(-1);
+    this.allImages = ko.observableArray();
+    this.auto = ko.observable(false);
+    this.start = ko.computed({
+        read: function (timeout)
+        {
+            if (_this.auto() === true)
+            {
+                setTimeout(_this.next, timeout);
+            }
+        },
+        write: function (timeout)
+        {
+            if (_this.auto() === true)
+            {
+                setTimeout(_this.next, timeout);
+            }
+        }
+    });
+    this.selectedImage = ko.computed(function ()
+        {
+            if (_this.allImages)
+            {
+
+                return _this.allImages()[_this.currentImageIndex()];
+            }
+        });
+    this.next = function ()
+    {
+        $("#displayImage").fadeOut(500, function ()
+        {
+            if (_this.currentImageIndex() < _this.allImages().length - 1)
+            {
+                _this.currentImageIndex(_this.currentImageIndex() + 1);
+            }
+            else
+            {
+                _this.currentImageIndex(0);
+            }
+            $("#displayImage").fadeIn();
+            _this.start(2000);
+        });
+    }
+    this.previous = function ()
+    {
+        $("#displayImage").fadeOut(500, function ()
+        {
+            if (_this.currentImageIndex() > 0)
+            {
+                _this.currentImageIndex(_this.currentImageIndex() - 1);
+            }
+            else
+            {
+                _this.currentImageIndex(_this.allImages().length - 1);
+            }
+            $("#displayImage").fadeIn();
+        });
+    }
+}
+
+$(document).ready(function (data)
+{
+    var vm = new viewModel();
+    $.ajax({
+        url: "/api/Images/",
+        type:"GET"
+    }).done(function (data)
+    {
+        for (i = 0; i < data.length; i++)
+        {
+            vm.allImages.push(data[i]);
+        }
+        ko.applyBindings(vm);
+        vm.currentImageIndex(0);
+    });
+});
